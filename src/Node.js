473c11bdba99edc6cc8d9e3995944fba57ba0ab8
chiq2045 <charles.oroko@outlook.com>
2019-02-10 17:19:16 -0800

    Adds a Node object

---
+++ src/Node.js
@@ -0,0 +1,39 @@
+/** Class representing a node in a linked list */
+class Node {
+  /**
+   * Create a node
+   * @param {number} value - The node's value
+   */
+  constructor(value) {
+    this.prev = null;
+    this.value = value;
+    this.next = null;
+  }
+
+  /**
+   * Gets the value of the node
+   * @return {number} The node's value
+   */
+  getValue() {
+    return this.value;
+  }
+
+  /**
+   * Gets the next node
+   * @returns {Node} The next node
+   */
+  getNextNode() {
+    return this.next;
+  }
+
+  /** 
+   * Gets the previous node
+   * @returns {Node} The previous node
+   */
+  getPrevNode() {
+    return this.prev;
+  }
+
+}
+
+module.exports = Node;
